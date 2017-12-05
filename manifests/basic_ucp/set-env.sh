6ec8b26de26acafde6211e83a899fcf25d25875b
Scott Hussey <sh8121@att.com>
2017-12-05 09:39:00 -0600

    Update Armada template for job cleanup
    - Add delete hooks for jobs to UCP services
    - Add a few additional configuration options
    - Add 'helm init' to run tiller after genesis
    - Add script to get a Keystone token if needed
    
    Change-Id: Ie27c295c0f6803490dfdcf969a70aa9a886e072b

--- manifests/basic_ucp/set-env.sh
+++ manifests/basic_ucp/set-env.sh
@@ -5,5 +5,6 @@ export GENESIS_NODE_IP=172.24.1.100
 export MASTER_NODE_IP=172.24.1.101
 export NODE_NET_IFACE=ens3
 export PROMENADE_IMAGE=quay.io/attcomdev/promenade:master
-export ARMADA_IMAGE=quay.io/attcomdev/armada:v0.7.1
+export ARMADA_IMAGE=quay.io/attcomdev/armada:master
 export DRYDOCK_IMAGE=sthussey/drydock:386668v14
+export MAAS_CACHE_IMAGE=sthussey/maas-cache:cachetest
