5ada2ba93758b2819ced0b4a6fe81af90e8ec96e
Anthony Lin <anthony.jclin@gmail.com>
2018-05-15 08:29:04 +0000

    [W.I.P] Multi-Nodes Gate
    Enable multi-node gate for UCP Integration using core UCP
    components.
    
    Deployment steps will be based on the README file in [0]. Site
    deployment will be driven by Shipyard.
    
    We will re-use and adapt the source codes from the Promenade
    g2 Gate [1].
    
    [0] https://review.gerrithub.io/#/c/att-comdev/ucp-integration/+/409541
    [1] https://github.com/att-comdev/promenade/tree/master/tools/g2
    
    Change-Id: Ia7958f8426f2b9ffd185735ab2c6688c4154849e

---
+++ tools/ucp_gate/stages/genesis.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+set -e
+
+source "${GATE_UTILS}"
+
+cd "${TEMP_DIR}"
+mkdir scripts
+
+# Create setup_genesis.sh script
+cat << EOF > ${TEMP_DIR}/scripts/setup_genesis.sh
+#!/bin/bash
+
+mkdir -p /root/deploy
+cd /root/deploy && git clone http://github.com/att-comdev/ucp-integration
+cd /root/deploy/ucp-integration && git fetch https://review.gerrithub.io/att-comdev/ucp-integration refs/changes/41/409541/1 && git checkout FETCH_HEAD
+cp /root/ucp/ucp_drydock_kvm_ssh_key.yaml /root/deploy/ucp-integration/site/gate-multinode/secrets/passphrases/ucp_drydock_kvm_ssh_key.yaml
+cd /root/deploy/ucp-integration/manifests/gate_multinode && source set-env.sh && ./deploy-ucp.sh
+
+EOF
+
+# Create ucp_drydock_kvm_ssh_key.yaml
+cat << EOF > ${TEMP_DIR}/scripts/ucp_drydock_kvm_ssh_key.yaml
+---
+schema: deckhand/CertificateKey/v1
+metadata:
+  schema: metadata/Document/v1
+  name: ucp_drydock_kvm_ssh_key
+  layeringDefinition:
+    layer: site
+    abstract: false
+  storagePolicy: cleartext
+data: |-
+EOF
+
+# Copy virtmgr private key
+cp /home/virtmgr/.ssh/id_rsa ${TEMP_DIR}/scripts/virtmgr_id_rsa
+
+# Add a space in front so that we can append the content to ucp_drydock_kvm_ssh_key.yaml
+# Append virtmgr private key to ucp_drydock_kvm_ssh_key.yaml
+# Delete the virtmgr private key that is in the temporary directory
+sed -i -e 's/^/ /' ${TEMP_DIR}/scripts/virtmgr_id_rsa
+cat ${TEMP_DIR}/scripts/virtmgr_id_rsa >> ${TEMP_DIR}/scripts/ucp_drydock_kvm_ssh_key.yaml
+echo "..." >> ${TEMP_DIR}/scripts/ucp_drydock_kvm_ssh_key.yaml
+rm ${TEMP_DIR}/scripts/virtmgr_id_rsa
+
+# Change permissions of setup_genesis.sh script
+chmod 777 ${TEMP_DIR}/scripts/setup_genesis.sh
+
+# Copies script and virtmgr private key to genesis VM
+rsync_cmd "${TEMP_DIR}/scripts/setup_genesis.sh" "${GENESIS_NAME}:/root/ucp/"
+rsync_cmd "${TEMP_DIR}/scripts/ucp_drydock_kvm_ssh_key.yaml" "${GENESIS_NAME}:/root/ucp/"
+
+set -o pipefail
+ssh_cmd "${GENESIS_NAME}" /root/promenade/setup_genesis.sh 2>&1 | tee -a "${LOG_FILE}"
+set +o pipefail
+
+if ! ssh_cmd n0 docker images | tail -n +2 | grep -v registry:5000 ; then
+    log_warn "Using some non-cached docker images.  This will slow testing."
+    ssh_cmd n0 docker images | tail -n +2 | grep -v registry:5000 | tee -a "${LOG_FILE}"
+fi
